52f1a30c180f9686cee9b3c3f4690f32ad98a9a3
Kriess Dukes <kdukes282@noscihigh.org>
2021-03-26 05:05:53 +0000

    I added the codes for the ones I understood. I'll ask you about the rest of them tomorrow.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 //Create a variable called myObject and assign 
 //an object to it.
 
+myObject = ()
 
 
 
 //Add two properties of firstName and lastName
 //using dot notation
 
+myObject = firstName.lastName
 
 
 
@@ -14,12 +16,13 @@
 //using bracket notation
 //hobbies must be an array of at least 4 strings
 
-
+myObject(hobbies, age)
+hobbies = ['dancing, producing, singing, watching anime']
 
 
 //Print the myObject variable
 
-
+print(myObject)
 
 
 //Use a for in loop to print all of the keys of the object
@@ -39,3 +42,5 @@ create an object that is about a specific car
 Include: year, make, model, color and an array of features that you want your car to have
 
 */
+
+object = (Bugatti Divo, 8/24/2018, orange and black, v12 engine with extra long transmission)
